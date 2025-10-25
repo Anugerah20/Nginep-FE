7bbf984b529aeedfddd02fed6735bca8dcd72ed7
anugerah20 <nabilpangestu123@gmail.com>
2025-10-25 18:18:31 +0700

    refactor: update detail hotel front page

--- src/app/(front)/hotel/[slug]/page.tsx
+++ src/app/(front)/hotel/[slug]/page.tsx
@@ -97,7 +97,7 @@ export default async function DetailHotel({ params }: Props) {
             <iframe
               className="h-full w-full border-0"
               frameBorder={0}
-              src={`https://www.google.com/maps/embed/v1/place?q=${hotel.title},+Bali&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}
+              src={`https://www.google.com/maps/embed/v1/place?q=${hotel.title},+Bali&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API}`}
               allowFullScreen
             />
           </div>
